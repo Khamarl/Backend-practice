efd6ea4eb9ec0d6776869bb22badecd2a4dccc8d
Khamarl C <khamarlchristie@hotmail.co.uk>
2022-09-26 16:37:00 +0100

    restructed get id into try catch block

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const beasts = require("./beasts");
 const cors = require("cors");
+const logRoute = require("./route-logger")
 // Make a basic server 
 const app = express();
 
@@ -10,6 +11,10 @@ app.use(cors());
 
 // Tell express to always read the body of POST requests
 app.use(express.json())
+
+//add middleware to log routes
+
+app.use(logRoute);
 
 // Set up server routes 
 app.get("/", (request, response) => {
@@ -29,12 +34,23 @@ app.get("/beasts/random", (req, res) => {
 });
 app.get("/beasts/:id", (req, res) => {
 
+    try { //Attempt to do something, stops at error if there is one
+        // convert id into an int
+        const id =parseInt(req.params.id)
 
-    if(0 <= req.params.id && req.params.id < beasts.length){
-   const filtered = beasts.filter((beast) => beast.id == req.params.id )
-   res.send(filtered[0])
-    } else {
-        res.status(400).send({error: "You messed up"})
+        // if id is nan, exit try and throw error 
+        if(!id && id !== 0) {
+            throw "Invalid input"
+        } else if(id < 0 || id >= beasts.length) {
+            throw "No such beast"
+        }
+        // if(0 <= req.params.id && req.params.id < beasts.length){
+            const filtered = beasts.filter((beast) => beast.id == req.params.id )
+            res.send(filtered[0])
+        // if there was a problem anywhere in the try, take error information 
+    } catch (e) {
+        // send a response explaining the issue 
+        res.status(400).send({error: e})
     }
 
 });
